7bf031a988cbc49640f487aa755ec9f09589427b
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-12-12 15:25:28 -0500

    Added _TSWSearchAndReplace Plugin

---
+++ _TSWSearchAndReplace/README.md
@@ -0,0 +1,10 @@
+# _TSWSearchAndReplace
+React Wordpress Microsite plugin for replacing any text references to The Smart Wallet with the appropriate site name.
+
+This plugin also replaces the `contact-us` link with an `mailto:info@` link.
+
+This plugin uses the `the_content` filter to do the string replacement.
+
+## Installation
+From the command line, run `react-wp --plugin --install _TSWSearchAndReplace`.
+Once installed, select `Y` when prompted to activate.
---
+++ _TSWSearchAndReplace/index.js
@@ -0,0 +1,34 @@
+import {Component} from 'react';
+import HookStore from '@Core/HookStore';
+
+export class _TSWSearchAndReplace extends Component {
+
+    replaceContent = (content) => {
+        // TSW lazyloads images, we don't
+        content = content.replace(/data-src/g, 'src');
+
+        // Clear any references to TSW
+        content = content.replace(/the smart wallet/gi, process.env.REACT_APP_DEFAULT_SITENAME);
+        content = content.replace(/thesmartwallet/gi, process.env.REACT_APP_DEFAULT_SITENAME.replace(/ /g, ''));
+
+        // Replace contact-us with mailto: link
+        let tswContactRegex = /(http:\/\/|https:\/\/)(thesmartwallet\.com\/contact-us)/gi;
+        // strip out www. and query strings
+        let mailto = `mailto:info@${window.location.host.replace('www.', '').split('?')[0]}`;
+        content = content.replace(tswContactRegex, mailto);
+
+        // Update internal links
+        let tswAnchorRegex = /(\S*?)href=(["'])(http:\/\/|https:\/\/)(thesmartwallet\.com)\1/gi;
+        content = content.replace(tswAnchorRegex, `href="${window.location.host}/posts`);
+
+        return content;
+    }
+
+    componentDidMount(){
+        HookStore.addFilter( 'the_content', 'Post', this.replaceContent, 1 );
+    }
+
+    render(){
+        return(null);
+    }
+}
