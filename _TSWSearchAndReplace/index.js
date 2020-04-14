c9107a3ae5b7044b04d1a97f98987052a42b63b7
Devon Wieczorek <devon.wieczorek93@gmail.com>
2020-04-14 13:26:15 -0400

    Fixed internal link replacement

--- _TSWSearchAndReplace/index.js
+++ _TSWSearchAndReplace/index.js
@@ -10,28 +10,27 @@ export class _TSWSearchAndReplace extends Component {
         // Clear any references to TSW
         content = content.replace(/the smart wallet/gi, process.env.REACT_APP_DEFAULT_SITENAME);
 
-        // First we replace textual representations of thesmartwallet
-        let host = process.env.REACT_APP_DEFAULT_SITENAME.replace(/ /g, '')
+        // Update internal links
+        let tswAnchorRegex = /(\S*?)href=(["'])(http:\/\/|https:\/\/)(thesmartwallet\.com)\1/gi;
+        content = content.replace(tswAnchorRegex, `href="${window.location.host}/posts`);
+
+        // Replace textual representations of thesmartwallet
+        let host = process.env.REACT_APP_DEFAULT_SITENAME.replace(/ /g, '');
         content = content.replace(/thesmartwallet/gi, host);
 
-        // Then we need to put if back for any image src or srcset
+        // Then we need to put it back for any image src or srcset
         let srcExp = new RegExp(`(src=".*|srcset=".*)${host}`, 'g', 'i');
         let hostExp = new RegExp(host, 'gi');
-        content = content.replace(srcExp, ($1) => $1.replace(hostExp, 'thesmartwallet'))
+        content = content.replace(srcExp, ($1) => $1.replace(hostExp, 'thesmartwallet'));
         // (?<!src=".*) - make sure to not replace image sources
         // Negative lookbehinds not yet supported, although this is the cleaner way to do things vvv
         // content = content.replace(/(?<!src=".*)thesmartwallet/gi, process.env.REACT_APP_DEFAULT_SITENAME.replace(/ /g, ''));
-
 
         // Replace contact-us with mailto: link
         let tswContactRegex = /(http:\/\/|https:\/\/)(thesmartwallet\.com\/contact-us)/gi;
         // strip out www. and query strings
         let mailto = `mailto:info@${window.location.host.replace('www.', '').split('?')[0]}`;
         content = content.replace(tswContactRegex, mailto);
-
-        // Update internal links
-        let tswAnchorRegex = /(\S*?)href=(["'])(http:\/\/|https:\/\/)(thesmartwallet\.com)\1/gi;
-        content = content.replace(tswAnchorRegex, `href="${window.location.host}/posts`);
 
         return content;
     }
