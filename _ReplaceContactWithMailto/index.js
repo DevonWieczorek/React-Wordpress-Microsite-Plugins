f7a4fe8862babc9279ecc7018e1aa070bc4f93b5
Devon Wieczorek <devon.wieczorek93@gmail.com>
2020-04-29 14:50:36 -0400

    Replace with and without search params

--- _ReplaceContactWithMailto/index.js
+++ _ReplaceContactWithMailto/index.js
@@ -8,14 +8,18 @@ export class _ReplaceContactWithMailto extends Component {
 
         // TSW contact us link
         const tswContactRegex = /(http:\/\/|https:\/\/)(thesmartwallet\.com\/contact-us)/gi;
+        const tswContactSearchRegex = new RegExp(`(http://|https://)(thesmartwallet.com/contact-us${window.location.search})`);
 
         // strip out www. and query strings from host
         const urlHost = window.location.host.replace('www.', '').split('?')[0];
         const urlHostRegex = new RegExp(`${window.location.protocol}//${urlHost}/posts/contact-us`, 'gi');
+        const urlHostSearchRegex = new RegExp(`${window.location.protocol}//${urlHost}/posts/contact-us${window.location.search}`, 'gi');
 
         // Replace both tsw and this site, as to not be dependent on _TSWSearchAndReplace
         const mailto = `mailto:info@${urlHost}`;
+        content = content.replace(tswContactSearchRegex, mailto);
         content = content.replace(tswContactRegex, mailto);
+        content = content.replace(urlHostSearchRegex, mailto);
         content = content.replace(urlHostRegex, mailto);
 
         return content;
