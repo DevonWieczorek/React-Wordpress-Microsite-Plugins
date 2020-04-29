c74f10cf3810f27b15c9c98d495f3ef3b1ab4f2e
Devon Wieczorek <devon.wieczorek93@gmail.com>
2020-04-29 13:59:17 -0400

    Removed _ReplaceContactWithMailto functionality from _TSWSearchAndReplace

--- _TSWSearchAndReplace/README.md
+++ _TSWSearchAndReplace/README.md
@@ -1,8 +1,6 @@
 # _TSWSearchAndReplace
 React Wordpress Microsite plugin for replacing any text references to The Smart Wallet with the appropriate site name.
 
-This plugin also replaces the `contact-us` link with an `mailto:info@` link, as well as hiding the post date from the Privacy Policy and T&C pages since they include the date in their content.
-
 This plugin uses the `the_content` filter to do the string replacement.
 
 
--- _TSWSearchAndReplace/index.js
+++ _TSWSearchAndReplace/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import {Component} from 'react';
 import HookStore from '@Core/HookStore';
 
 export class _TSWSearchAndReplace extends Component {
@@ -13,22 +13,17 @@ export class _TSWSearchAndReplace extends Component {
         content = content.replace(tswAnchorRegex, `href="${window.location.protocol}//${window.location.host}/posts`);
 
         // Replace textual representations of thesmartwallet
-        let host = process.env.REACT_APP_DEFAULT_SITENAME.replace(/ /g, '');
-        content = content.replace(/thesmartwallet/gi, host);
+        const envHost = process.env.REACT_APP_DEFAULT_SITENAME.replace(/ /g, '');
+        content = content.replace(/thesmartwallet/gi, envHost);
 
         // Then we need to put it back for any image src or srcset
-        let srcExp = new RegExp(`(src=".*|srcset=".*)${host}`, 'g', 'i');
-        let hostExp = new RegExp(host, 'gi');
+        let srcExp = new RegExp(`(src=".*|srcset=".*)${envHost}`, 'g', 'i');
+        let hostExp = new RegExp(envHost, 'gi');
         content = content.replace(srcExp, ($1) => $1.replace(hostExp, 'thesmartwallet'));
+
         // (?<!src=".*) - make sure to not replace image sources
         // Negative lookbehinds not yet supported, although this is the cleaner way to do things vvv
         // content = content.replace(/(?<!src=".*)thesmartwallet/gi, process.env.REACT_APP_DEFAULT_SITENAME.replace(/ /g, ''));
-
-        // Replace contact-us with mailto: link
-        let tswContactRegex = /(http:\/\/|https:\/\/)(thesmartwallet\.com\/contact-us)/gi;
-        // strip out www. and query strings
-        let mailto = `mailto:info@${window.location.host.replace('www.', '').split('?')[0]}`;
-        content = content.replace(tswContactRegex, mailto);
 
         return content;
     }
