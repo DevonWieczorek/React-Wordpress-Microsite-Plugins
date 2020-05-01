230a8136f0053f41cf9caee8556895df4041bcc2
Devon Wieczorek <devon.wieczorek93@gmail.com>
2020-05-01 15:33:30 -0400

    Fix internal links independent of _TSWSearchAndReplace

--- _TSWTips/index.js
+++ _TSWTips/index.js
@@ -3,9 +3,18 @@ import HookStore from '@Core/HookStore';
 
 export class _TSWTips extends Component {
 
+    // We don't need _TSWSearchAndReplace but we do need to fix internal links
+    fixInternalLinks = content => {
+        let tswAnchorRegex = /(\S*?)href=(["'])(http:\/\/|https:\/\/)(thesmartwallet\.com)\1/gi;
+        content = content.replace(tswAnchorRegex, `href="${window.location.protocol}//${window.location.host}/posts`);
+        return content;
+    }
+
     componentDidMount(){
-        // Enqueue the stylesheet using it's public location
+        // Enqueue the stylesheet using its public location
         HookStore.doAction('enqueue_styles', 'tswtips', `${process.env.PUBLIC_URL}/styles/tsw-tips.css`, '1.0', true);
+
+        HookStore.addFilter( 'the_content', 'tswtips', this.fixInternalLinks, 1 );
     }
 
     render(){
--- _Translate/README.md
+++ _Translate/README.md
@@ -2,7 +2,7 @@
 React Wordpress Microsite plugin for filtering posts by language for a given user. When a user clicks the link for a translated article, that language is stored as their preferred language.
 
 ## Installation
-From the command line, run `react-wp --plugin --install _TSWTips`.
+From the command line, run `react-wp --plugin --install _Translate`.
 Once installed, select `Y` when prompted to activate.
 
 Then, manually copy the `public/translate.js` file from the plugin to the `public/scripts` directory in your project.
